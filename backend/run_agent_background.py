7d5835db4f8f08e54c1041822fab69d43d7bc9c3
iaisep <iallamadas@universidadisep.com>
2025-05-29 17:59:27 -0300

    modified:   backend/run_agent_background.py fix indentations

--- backend/run_agent_background.py
+++ backend/run_agent_background.py
@@ -20,8 +20,15 @@ rabbitmq_host = os.getenv('RABBITMQ_HOST', 'rabbitmq')
 rabbitmq_port = int(os.getenv('RABBITMQ_PORT', 5672))
 rabbitmq_user = os.getenv('RABBITMQ_USER', 'guest')
 rabbitmq_password = os.getenv('RABBITMQ_PASSWORD', 'guest')
-rabbitmq_broker = RabbitmqBroker(host=rabbitmq_host,port=rabbitmq_port,virtual_host="/",username=rabbitmq_user,
-    password=rabbitmq_password, middleware=[dramatiq.middleware.AsyncIO()])
+rabbitmq_broker = RabbitmqBroker(
+    host=rabbitmq_host,
+    port=rabbitmq_port,
+    virtual_host="/",
+    username=rabbitmq_user,
+    password=rabbitmq_password,
+    middleware=[dramatiq.middleware.AsyncIO()]
+)
+
 dramatiq.set_broker(rabbitmq_broker)
 
 _initialized = False
